b6e23190d55dbbc90016be368a532684d0973566
Pit McZagor <70600463+pitzagor@users.noreply.github.com>
2024-07-13 19:48:55 +0100

    Update expandobot.py

--- expandobot.py
+++ expandobot.py
@@ -36,4 +36,8 @@ def slack_events():
 
 # Start your app
 if __name__ == "__main__":
-    flask_app.run(port=int(os.environ.get("PORT", 3000)))
+    # Get port from environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+
+    # Start the Flask app
+    app.run(host="0.0.0.0", port=port)
